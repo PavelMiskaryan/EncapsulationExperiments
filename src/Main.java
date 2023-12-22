4dd2fc2dc2039e4f14179287b55699389e781016
Pavel Miskaryan <pavelinvoker5k@gmail.com>
2023-12-23 00:08:16 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        ChildBankAccount account =
+                new ChildBankAccount(10000);
+        account.depositMoney(1000);
+        account.depositMoney(2000);
+        account.depositMoney(10000);
+        account.depositMoney(-1000);
+        System.out.println("Balance: " +
+                account.getBalance());
+
+        account.debitMoney(500);
+        account.debitMoney(422.75);
+        account.debitMoney(50000);
+        account.debitMoney(-50);
+        System.out.println("Balance: " +
+                account.getBalance());
+    }
+}
+
